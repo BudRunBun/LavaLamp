48d446fcd88a46e3bfc146c5783ba6a93b48f2dd
BudRunBun <chest685@gmail.com>
2020-09-23 20:15:44 +0300

    Guard entity updated

--- src/main/java/com/budrunbun/lavalamp/model/GuardHeldItemLayer.java
+++ src/main/java/com/budrunbun/lavalamp/model/GuardHeldItemLayer.java
@@ -10,10 +10,13 @@ import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.HandSide;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nonnull;
 
 @SuppressWarnings("deprecation")
+@OnlyIn(Dist.CLIENT)
 public class GuardHeldItemLayer extends LayerRenderer<GuardEntity, GuardModel> {
     private final IEntityRenderer<GuardEntity, GuardModel> entityRenderer;
 
@@ -26,31 +29,23 @@ public class GuardHeldItemLayer extends LayerRenderer<GuardEntity, GuardModel> {
         if (guard.isAggressive()) {
             ItemStack shield = guard.getHeldItemOffhand();
             ItemStack sword = guard.getHeldItemMainhand();
-
-            if (!shield.isEmpty()) {
-                GlStateManager.pushMatrix();
-
-                int angle = guard.getRevengeTarget() != null ? (guard.getRevengeTarget().getDistance(guard) < 2 ? 90 : 0) : 0;
-
-                GlStateManager.rotated(angle, 0, 1, 0);
-                renderHeldItem(guard, shield, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, HandSide.LEFT);
-
-                GlStateManager.popMatrix();
-            }
-            renderHeldItem(guard, shield, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, HandSide.RIGHT);
+            int angle = guard.getRevengeTarget() != null ? (guard.getRevengeTarget().getDistance(guard) < 2 ? 90 : 0) : 0;
+            renderHeldItem(guard, shield, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, HandSide.LEFT, angle);
+            renderHeldItem(guard, sword, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, HandSide.RIGHT, 0);
         }
     }
 
-    private void renderHeldItem(LivingEntity guard, ItemStack stack, ItemCameraTransforms.TransformType transformType, HandSide handSide) {
+    private void renderHeldItem(LivingEntity guard, ItemStack stack, ItemCameraTransforms.TransformType transformType, HandSide handSide, int degrees) {
         if (!stack.isEmpty()) {
             GlStateManager.pushMatrix();
             if (guard.shouldRenderSneaking()) {
                 GlStateManager.translatef(0.0F, 0.2F, 0.0F);
             }
-
-            this.translateToHand(handSide);
+            System.out.println("Rotate for " + degrees + " degrees");
+            translateToHand(handSide);
             GlStateManager.rotatef(-90.0F, 1.0F, 0.0F, 0.0F);
             GlStateManager.rotatef(180.0F, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotatef(degrees, 0, 1, 0);
             boolean flag = handSide == HandSide.LEFT;
             GlStateManager.translatef((float) (flag ? -1 : 1) / 16.0F, 0.125F, -0.625F);
             Minecraft.getInstance().getFirstPersonRenderer().renderItemSide(guard, stack, transformType, flag);
@@ -62,6 +57,7 @@ public class GuardHeldItemLayer extends LayerRenderer<GuardEntity, GuardModel> {
         ((IHasArm) this.getEntityModel()).postRenderArm(0.0625F, side);
     }
 
+    @Override
     @Nonnull
     public GuardModel getEntityModel() {
         return this.entityRenderer.getEntityModel();
